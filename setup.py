e092fafeb6e364d3c7d46c74772eda5bd9f779ee
datahelpssai <saisantoshavala@gmail.com>
2024-03-25 17:43:29 -0400

    requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'avala',
+    author_email= 'avalasai123@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
